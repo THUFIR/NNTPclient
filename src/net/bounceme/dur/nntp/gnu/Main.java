2f768825657585e76af1d74faf9ea27eb1b1e157
Thufir Hawat <hawat.thufir@gmail.com>
2013-03-23 04:07:07 -0700

    increment

---
+++ src/net/bounceme/dur/nntp/gnu/Main.java
@@ -0,0 +1,23 @@
+package net.bounceme.dur.nntp.gnu;
+
+//hmm
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private final static Logger LOG = Logger.getLogger(Main.class.getName());
+    Usenet u = Usenet.INSTANCE;
+
+    public Main() throws Exception {
+        LOG.info("in constructor");
+        u.connect();
+        Page page = new Page();
+        u.getMessages(page);
+    }
+
+    public static void main(String... args) throws Exception {
+        LOG.info("well here we are");
+        new Main();
+    }
+}
